a2fa7754b37ed88f5fb9ab41b6ab5333fca09093
max529 <maxime.betrisey@hotmail.com>
2024-04-27 11:39:42 +0200

    Change main frame and add ws

---
+++ Websocket/Routes/${{DefaultApp}}UnitRoute.cs
@@ -0,0 +1,18 @@
+using AventusSharp.Data.Manager;
+using AventusSharp.WebSocket;
+using AventusSharp.WebSocket.Attributes;
+using Core.Websocket;
+using ${{DefaultApp}}.Data;
+using ${{DefaultApp}}.Logic.DM;
+
+namespace ${{DefaultApp}}.Websocket.Routes {
+
+    [EndPoint<MainEndPoint>("${{DefaultApp}}")]
+    public class ${{DefaultApp}}UnitRoute : StorableWsRoute<${{DefaultApp}}Unit>
+    {
+        protected override IGenericDM<${{DefaultApp}}Unit>? GetDM()
+        {
+            return ${{DefaultApp}}UnitDM.GetInstance();
+        }
+    }
+}
